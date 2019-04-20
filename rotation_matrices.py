c67c703e55407c697d64233ab4d83287d13ee2d3
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-04-21 00:23:57 +0800

    Delete rotation_matrices.py

--- rotation_matrices.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def rotx(theta):
-    rot = np.array([[1 0 0], [0 1 0], [0 0 1]])
-    rot(2, 2) = np.cos(theta)
-    rot(2, 3) = -np.sin(theta)
-    rot(3, 2) = np.sin(theta)
-    rot(3, 3) = np.cos(theta)
-    return rot
