83ec10efa9692b66148f59771da08874bf4c7e1c
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-05-16 22:38:40 +0800

    Add files via upload

---
+++ RLAnIntro/RLAnIntro_Chap2_Multi_Armed_Bindits.py
@@ -0,0 +1,93 @@
+#############################################################
+# Based on scripts of Shangtong Zhang, Tian Jun,            #
+# Artem Oboturov, Kenta Shimada                             #
+# Reinforcement Learning: An Introduction Chapter 2,        #
+# Multi-armed Bandits Problem                               #
+#############################################################
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+matplotlib.use('Agg')
+
+
+class Bandit:
+    def __init__(self, k_arm=10, epsilon=0., initial=0, step_size=0.1,
+                 sample_averages=False, UCB_param=None, gradient=False,
+                 gradient_baseline=False, true_reward=0.):
+        self.k = k_arm
+        self.step_size = step_size
+        self.sample_average = sample_averages
+        self.indices = np.arange(self.k)
+        self.time = 0
+        self.UCB_param = UCB_param
+        self.gradient = gradient
+        self.gradient_baseline = gradient_baseline
+        self.average_reward = true_reward
+        self.epsilon = epsilon
+        self.initial = initial
+
+    def reset(self):
+        self.q_true = np.random.randn(self.k) + self.true_reward
+        self.q_estimation = np.zeros(self.k) + self.initial
+        self.action_count = np.zeros(self.k)
+        self.best_action = np.argmax(self.q_true)
+
+    def act(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.choice(self.indices)
+
+        if self.UCB_param is not None:
+            UCB_estimation = self.q_estimation + self.UCB_param * np.sqrt(np.log(self.time + 1))\
+                             / (self.action_count + le-5)
+            q_best = np.max(UCB_estimation)
+            return np.random.choice([action for action, q in enumerate(UCB_estimation) if q == q_best])
+
+        if self.gradient:
+            exp_est = np.exp(self.q_eatimaiton)
+            self.action_prob = exp_est / np.sum(exp_est)
+            return np.random.choice(self.indices, p=self.action_prob)
+
+        q_best = np.max(self.q_estimation)
+        return np.random.choice([action for action, q in enumerate(UCB_estimation) if q == q_best])
+
+    def step(self, action):
+        reward = np.random.randn() + self.q_true[action]
+        self.time += 1
+        slef.average_reward = ((self.time - 1.0) * self.average_reward + reward) / self.time
+        self.action_count[action] += 1
+
+        if self.sample_average:
+            self.q_eatimation[action] += 1.0 / self.action_count[action] * (reward - self.q_estimation[action])
+        elif self.gradient:
+            one_hot = np.zeros(self.k)
+            one_hot[action] = 1
+            if self.gradient_baseline:
+                baseline = self.average_reward
+            else:
+                baseline = 0
+            self.q_estimation = self.q_estimation + self.step_size * (reward - baseline) * (one_hot - self.action_prob)
+        else:
+            self.q_estimation[action] += self.step_size * (reward - self.q_estimation[action])
+        return reward
+
+
+def simulate(runs, time, bandits):
+    best_action_counts = np.zeros((len(bandits), runs, time))
+    rewards = np.zeros(best_action_counts.shape)
+
+    for i, bandit in enumerate(bandits):
+        for r in tqdm(range(runs)):
+            bandit.reset()
+            for t in range(time):
+                action = bandit.act()
+                reward = bandit.step(action)
+                rewards[i, r, t] = reward
+                if action == bandit.best_action
+                    best_action_counts[i, r, t] = 1
+    best_action_counts = best_action_counts.mean(axis=1)
+    rewards = rewards.mean(axis=1)
+    return best_action_counts, rewards
+
+
