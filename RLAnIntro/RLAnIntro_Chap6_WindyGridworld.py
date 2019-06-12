2cc9e99b5c6644c4de3c8132fba2ca88740d7838
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-06-12 22:06:01 +0800

    Add files via upload

--- RLAnIntro/RLAnIntro_Chap6_WindyGridworld.py
+++ RLAnIntro/RLAnIntro_Chap6_WindyGridworld.py
@@ -1,3 +1,16 @@
+#######################################################################
+# Copyright (C)                                                       #
+# 2016-2018 Shangtong Zhang(zhangshangtong.cpp@gmail.com)             #
+# 2016 Kenta Shimada(hyperkentakun@gmail.com)                         #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+#############################################################
+# Based on scripts of Shangtong Zhang, Tian Jun,            #
+# Artem Oboturov, Kenta Shimada                             #
+# Reinforcement Learning: An Introduction Chapter 6- Sarsa  #
+# On-policy TD control                                      #
+#############################################################
 import numpy as np
 import matplotlib.pyplot as plt
 
