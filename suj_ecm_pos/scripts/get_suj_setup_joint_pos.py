e2946a159a325d4c09c0e30524fd6dc7fb1363e5
Jing HUANG <35371389+HuangJingGitHub@users.noreply.github.com>
2019-07-25 12:51:36 +0800

    Add files via upload

--- suj_ecm_pos/scripts/get_suj_setup_joint_pos.py
+++ suj_ecm_pos/scripts/get_suj_setup_joint_pos.py
@@ -1,25 +1,27 @@
 import serial
+import time
 import numpy as np
 import copy
 
-ser = serial.Serial('/dev/ttyUSB0', baudrate=115200)
+ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=0.2, xonxoff=False, rtscts=False,
+                    write_timeout=0.5, dsrdtr=False, inter_byte_timeout=None, exclusive=None)
 
 POT_Condition = [1, 1, 1, 1, 1, 1,
                  0, 1, 1, 1, 1, 1]
 full_range = int('FFFFFF', 16)
-pi = 3.1415926
+pi = 3.14159265359
 joint_offset_suj1 = [-0.115, -pi, -pi, -pi, -pi, -pi]
 joint_offset_suj2 = [-0.109, -pi, -pi, -pi, -pi, -pi]
 joint_offset_ecm = [-0.141, -pi, -pi, -pi, -pi, -pi]
 
 def get_suj_joint_reading():
     readings = []
-    #voltages = np.zeros((12, 1)).tolist()
+    valid_reading = True
+    # voltages = np.zeros((12, 1)).tolist()
     voltages = [0 for i in range(12)]
     ser.write(b"AT+READALL\r\n")
     for i in range(13):
         readings.append(ser.read_until())
-        # print(readings)
         if i == 12:
             if readings[i][-4:] == b"OK\r\n":
                 print('Reading Success.')
@@ -32,9 +34,12 @@ def get_suj_joint_reading():
         POT = int(reading_[-3], 16)
         voltage = float(int(reading_[0:6], 16)) / float(full_range) * 2.5
         voltages[POT] = voltage
-    # print(voltages)
-    # print(readings)
-    return voltages
+    
+    if readings[0].decode('utf-8')[-3] == readings[11].decode('utf-8').[-3] :
+        valid_reading = False
+
+    print(readings)
+    return voltages, valid_reading
 
 def get_suj_joint_pos(voltages, suj_type):
     if suj_type == 'SUJ1':
@@ -46,11 +51,11 @@ def get_suj_joint_pos(voltages, suj_type):
     for joint_ in range(6):
         if joint_ == 0:
             if suj_type == 'SUJ1' or suj_type == 'SUJ2':
-                joint_pos[joint_] += (voltages[joint_] * POT_Condition[joint_] \
+                joint_pos[joint_] += (voltages[joint_] * POT_Condition[joint_] 
                                     + voltages[joint_+6] * POT_Condition[joint_+6]) / 2 \
                                     * (0.460422 if suj_type == 'SUJ1' else 0.462567)
             else:
-                joint_pos[joint_] += (voltages[joint_] * POT_Condition[joint_] \
+                joint_pos[joint_] += (voltages[joint_] * POT_Condition[joint_] 
                                     + voltages[joint_+6] * POT_Condition[joint_+6]) / 2 * 0.440917
         else:
             joint_pos[joint_] += (voltages[joint_] * POT_Condition[joint_] + voltages[joint_+6] * POT_Condition[joint_+6])\
@@ -98,7 +103,10 @@ def lock_brakes():
 
 
 def get_data():
-    reading = get_suj_joint_reading()
+    isValid = False
+    while !isValid:
+        [reading, isValid] = get_suj_joint_reading()
+    
     suj_joint = get_suj_joint_pos(reading, 'SUJ1')
     suj_joint_deg = [180 / pi * i for i in suj_joint]
     suj_joint_deg[0] = suj_joint[0]
